e81a7f811a2ae54d5cb6ca63f51ba9195b57f66c
ichika <806708480@qq.com>
2022-12-24 18:33:39 +0800

    移动端修改

--- js/main.js
+++ js/main.js
@@ -281,8 +281,8 @@ window.addEventListener('load', function () {
         if (window.innerWidth <= 990) {
             //移动端隐藏更多页面
             $('#container').attr('class', 'container');
-            $("#change").html("Hello&nbsp;World&nbsp;!");
-            $("#change1").html("一个建立于 21 世纪的小站，存活于互联网的边缘");
+            $("#change").html("Hello&nbsp;Gamer&nbsp;!");
+            $("#change1").html("这里是ichika，欢迎来访");
 
             //移动端隐藏弹窗页面
             $('#box').css("display", "none");
